c4a29ffc140de7ac6a06b0b4f8c0070bb6d2a577
Noob Science <78465651+newtoallofthis123@users.noreply.github.com>
2022-02-10 22:34:42 +0530

    v4 update ❤
    The new version 4 update with a modern ui

--- brain.js
+++ brain.js
@@ -1,7 +1,2 @@
-$(document).ready(function() {
-    function changeTagLine(){
-        $("#tag_line").text("Welcome to my little place on the web where I do something")
-    }
-    $("#tag_line").fadeOut(4800)
-    setTimeout(changeTagLine, 5000)
-});
+$(document).ready(function(){
+})
